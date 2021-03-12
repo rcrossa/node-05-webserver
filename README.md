41b30c9d2045eda963e5518d23df50272dff5b72
Roberto Rossa <rcrossa@hotmail.com>
2021-03-11 22:34:18 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # node-05-webserver
 
+1 - Al utilizar el proyecto la primera vez ejecutar los comandos:
+ * npm install
--- index.js
+++ index.js
@@ -1,18 +1,66 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const port = 3000;
 
-app.use(express.static(__dirname + '/public'));
+const port = process.env.PORT || 3000;
 
-app.get('/', function (req,res) {
-    res.send('Hola Mundo')
+app.use(express.json());
+// app.use(express.static(__dirname + '/public'));
 
-})
-app.post('/', function (req,res) {
-    res.send('Post Hola Mundo')
+app.get("/users", (req, res) => {
+  const users = [
+    {
+      id: 1,
+      name: "Roberto",
+    },
+    {
+      id: 2,
+      name: "Marta",
+    },
+  ];
+  res.json(users);
+});
 
-})
+app.post("/users", (req, res) => {
 
+  const user = req.body;
+  user.id = 86546 ;
+
+  const result = {
+    message: "User created",
+    user
+  };
+  res.status(201).json(result);
+});
+
+app.put("/users/:id", (req, res) => {
+
+    const { id } = req.params;
+    const user = req.body;
+
+    user.id = id;
+
+  const result = {
+    message: "User updated",
+    user
+  };
+  res.status(201).json(result);
+});
+
+app.patch("/users", (req, res) => {
+  const result = {
+    message: "User updated with patch",
+  };
+  res.json(result);
+});
+
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const result = {
+    message: `User ${id} deleted`,
+  };
+  res.json(result);
+});
 app.listen(port, () => {
-    console.log(`Server started - Port ${port}`);
-})
+  console.log(`Server started - Port ${port}`);
+});
